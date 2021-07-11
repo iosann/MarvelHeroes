d1cf6a877ec41c0702e66afb961a94e8b67e5d90
Anna Belousova <anna.belousova.2011@gmail.com>
2021-07-11 23:05:00 +0400

    Initial Commit

---
+++ MarvelHeroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelHeroes
+//
+//  Created by Anna Belousova on 11.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
